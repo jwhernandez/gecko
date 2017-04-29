6475d25b8b61f673f4b4d7f298d0ed71676300f2
pipio94 <joseherzu94@gmail.com>
2017-04-29 15:21:36 -0600

    todo

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Turisteando</title>
+    <title>Gecko Diabetes</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CDN Jquery-->
